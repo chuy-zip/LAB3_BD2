de0fe79f0c3542558b8c053dbe7d0d75e97436a1
chuy-zip <andreschuy03@gmail.com>
2025-02-07 17:20:06 -0600

    made connections

--- neo.py
+++ neo.py
@@ -1,2 +1,12 @@
-from neo4j import GraphDatabase
+from connection import get_neo_driver
 
+driver = get_neo_driver()
+
+try:
+    records, summary, keys = driver.execute_query(
+        "MERGE (p:Person {name: 'Call2'})",
+        database_="neo4j"
+    )
+    print("Query executed successfully")
+finally:
+    driver.close()
