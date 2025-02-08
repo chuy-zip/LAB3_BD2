4c3b37e7b61433712572671c21ba4e20b4587cd8
chuy-zip <andreschuy03@gmail.com>
2025-02-07 20:15:00 -0600

    function to create persons dinamycally

---
+++ person_creator.py
@@ -0,0 +1,28 @@
+from connection import get_neo_driver
+
+def create_person(driver, labels_list: list[str], attributes: dict):
+    labels = "Person"
+
+    if len(labels_list) > 0:
+
+        labels = labels + ":" + ":".join(labels_list)
+
+    combined_attributes = ", ".join([f"{key}: $attributes.{key}" for key in attributes.keys()])
+
+    query = f"MERGE (p:{labels} {{ {combined_attributes} }})"
+
+    print(labels)
+    print(combined_attributes)
+
+    records, summary, keys = driver.execute_query(
+        query,
+        attributes=attributes,
+        database_="neo4j"
+    )
+   
+    print(f"Created/updated node with labels {labels_list} and properties {attributes}")
+
+
+driver = get_neo_driver()
+
+create_person(driver, ["aYES", "NO2"], {"name": "Bob", "department": "HR"})
