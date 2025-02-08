afe6faab8d655d0b5464dc534c3eb8cecf0fa45e
Daniel Penedo <pen22217@uvg.edu.gt>
2025-02-08 12:59:48 -0600

    Update search.py

--- search.py
+++ search.py
@@ -39,3 +39,60 @@ def search_movie(driver, movie_title:str):
     for user in movies:
         print(f"{user._properties}")
     return movies
+
+def search_relation(driver, person_name: str) -> list:
+    records, summary, keys = driver.execute_query(
+        "MATCH (p:User)-[r]->(m:Movie) WHERE n.name = $q_name RETURN r",
+        q_name=person_name,
+        database_="neo4j"
+    )
+
+    if not records:
+        print(f"No user found with the name '{person_name}'.")
+        return None
+
+    relations = []
+    for record in records:
+        relation_node = record["r"]  
+        relations.append(relation_node) 
+
+    print(f"Found {len(relations)} relation(s) related to the name '{person_name}'.")
+    for relation in relations:
+        print(f"{relation}")
+    return relations    
+
+def search_user_movie(driver, person_name: str, movie_title: str) -> list:
+    """
+    Function that searches the relations between a person and a movie. Both of them are given by the user.
+
+    Args:
+        driver (driver): driver
+        person_name (string): The person's name
+        movie_title (string): The movie's title 
+
+    Returns:
+        (list): A list containing the whole relations.
+    """
+
+    records, summary, keys = driver.execute_query(
+        "MATCH (p:User)-[r]->(m:Movie) WHERE p.name = $q_name AND m:title = $q_title RETURN p, r, m",
+        q_name = person_name,
+        q_title = movie_title,
+        database = "neo4j"
+    )
+
+    if not records:
+        print(f"No relationships found between '{person_name}' and '{movie_title}'.")
+        return None
+    
+    relations = []
+    for record in records:
+        user_node = record["p"]
+        movie_node = record["m"]
+        relation_node = record["r"]
+        relations.append(user_node)
+        relations.append(relation_node)
+        relations.append(movie_node)
+
+    print(f"Found {len(relations)/3} relation(s) between '{person_name}' and '{movie_title}'.")
+    return relations
