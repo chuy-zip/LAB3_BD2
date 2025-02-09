ad76138679ebfc81fc94c4f91f66aa08ce96038f
Daniel Penedo <pen22217@uvg.edu.gt>
2025-02-08 20:22:06 -0600

    Update search.py

--- search.py
+++ search.py
@@ -19,6 +19,27 @@ def search_user(driver, person_name:str):
         print(f"{user._properties}")
     return users
 
+def search_person(driver, person_name:str):
+    records, summary, keys = driver.execute_query(
+        "MATCH (n:Person) WHERE n.name = $q_name RETURN n",
+        q_name=person_name,
+        database_="neo4j"
+    )
+
+    if not records:
+        print(f"No user found with the name '{person_name}'.")
+        return None
+
+    users = []
+    for record in records:
+        user_node = record["n"]  
+        users.append(user_node) 
+
+    print(f"Found {len(users)} person(people) with the name '{person_name}'.")
+    for user in users:
+        print(f"{user._properties}")
+    return users
+
 def search_movie(driver, movie_title:str):
     records, summary, keys = driver.execute_query(
         "MATCH (n:Movie) WHERE n.title = $q_title RETURN n",
